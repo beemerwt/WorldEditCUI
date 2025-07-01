61a144978d3d91fcee8bffd63d0d6c9c80dc1cec
Maddy Miller <git@madelinemiller.dev>
2025-07-01 19:28:58 +1000

    release: 1.21.7+01

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 allprojects {
     group = "org.enginehub.worldeditcui"
-    version = "${rootProject.libs.versions.minecraft.get()}+01-SNAPSHOT"
+    version = "${rootProject.libs.versions.minecraft.get()}+01"
 
     repositories {
         // mirrors:
